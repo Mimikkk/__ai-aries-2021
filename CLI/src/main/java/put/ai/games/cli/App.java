bbeb1dad60e64ef33d7e75b639b88c5889e98a76
Hououin Kyouma <inwazzin@gmail.com>
2022-01-17 02:14:29 +0100

    bot

--- CLI/src/main/java/put/ai/games/cli/App.java
+++ CLI/src/main/java/put/ai/games/cli/App.java
@@ -10,60 +10,63 @@ import put.ai.games.game.exceptions.RuleViolationException;
 import put.ai.games.rulesprovider.RulesProvider;
 
 import static put.ai.games.engine.loaders.MetaPlayerLoader.*;
+import static put.ai.games.game.Player.Color.*;
+import static put.ai.games.game.Player.getOpponent;
 
 public class App {
-    private static GameEngineImpl engine = null;
-    private static BoardFactory factory = null;
-    private static int timeout = 20000;
-    private static int boardSize = 8;
-    private static int firstWins = 0;
-    private static int secondWins = 0;
-    private static int draws = 0;
+  private static GameEngineImpl engine = null;
+  private static BoardFactory factory = null;
+  private static int timeout = 20000;
+  private static int boardSize = 8;
+  private static int firstWins = 0;
+  private static int secondWins = 0;
 
-    public static void main(String[] args) throws Exception {
-        Arrays.stream(args).forEach(System.out::println);
+  public static void main(String[] args) throws Exception {
+    System.out.println("Attributes:");
+    Arrays.stream(args).forEach(System.out::println);
 
-        var firstLoader = INSTANCE.load(args[0]).getDeclaredConstructor();
-        var secondLoader = INSTANCE.load(args[1]).getDeclaredConstructor();
-        factory = RulesProvider.INSTANCE.getRulesByName(args[2]);
-        if (args.length > 3) timeout = Integer.parseInt(args[3]);
-        if (args.length > 4) boardSize = Integer.parseInt(args[4]);
+    var firstLoader = INSTANCE.load(args[0]).getDeclaredConstructor();
+    var secondLoader = INSTANCE.load(args[1]).getDeclaredConstructor();
+    factory = RulesProvider.INSTANCE.getRulesByName(args[2]);
+    if (args.length > 3) timeout = Integer.parseInt(args[3]);
+    if (args.length > 4) boardSize = Integer.parseInt(args[4]);
 
-        factory.configure(new Hashtable<>() {{put(BoardFactory.BOARD_SIZE, boardSize);}});
+    factory.configure(new Hashtable<>() {{put(BoardFactory.BOARD_SIZE, boardSize);}});
 
-        var threadGroup = new ThreadGroup("players");
-        var first = new Wrapper(firstLoader.newInstance());
-        var second = new Wrapper(secondLoader.newInstance());
-        new Thread(threadGroup, first).start();
-        new Thread(threadGroup, second).start();
-        engine = new GameEngineImpl(factory) {{
-            setTimeout(timeout);
-            addPlayer(first);
-            addPlayer(second);
-        }};
+    var threadGroup = new ThreadGroup("players");
+    var first = new Wrapper(firstLoader.newInstance());
+    var second = new Wrapper(secondLoader.newInstance());
+    new Thread(threadGroup, first).start();
+    new Thread(threadGroup, second).start();
+    engine = new GameEngineImpl(factory) {{
+      setTimeout(timeout);
+      addPlayer(first);
+      addPlayer(second);
+    }};
 
-        for (var i = 0; i < 101; play()) {
-            if (++i % 5 != 0) continue;
-            System.out.printf("Iteration %d/%d\n", i, 100);
-            System.out.println("First wins: " + firstWins);
-            System.out.println("Second wins: " + secondWins);
-            System.out.println("Draws: " + draws);
-        }
-        System.exit(0);
+    System.out.println("Game started");
+    var start = System.currentTimeMillis();
+    for (var i = 0; i < 100; i++, play()) {
+      System.out.printf("Iteration %d/%d\n", i, 100);
+      System.out.println("First wins: " + firstWins);
+      System.out.println("Second wins: " + secondWins);
+      System.out.printf("Time spent: %.2fs\n", (System.currentTimeMillis() - start) / 1000.0);
+      start = System.currentTimeMillis();
     }
+    System.exit(0);
+  }
 
-    private static void play() {
-        Color winner = run();
-        firstWins += winner == Color.PLAYER1 ? 1 : 0;
-        secondWins += winner == Color.PLAYER2 ? 1 : 0;
-        draws += winner == Color.EMPTY ? 1 : 0;
-    }
+  private static void play() {
+    var winner = run();
+    firstWins += winner == PLAYER1 ? 1 : 0;
+    secondWins += winner == PLAYER2 ? 1 : 0;
+  }
 
-    private static Color run() {
-        try {
-            return engine.play((color, board, move) -> {});
-        } catch (RuleViolationException reason) {
-            return Player.getOpponent(reason.getGuilty());
-        }
+  private static Color run() {
+    try {
+      return engine.play((color, board, move) -> System.out.printf("%s move: %s\n", color, move));
+    } catch (RuleViolationException reason) {
+      return getOpponent(reason.getGuilty());
     }
+  }
 }
--- Players/NaivePlayer/src/main/java/put/ai/games/naiveplayer/NaivePlayer.java
+++ Players/NaivePlayer/src/main/java/put/ai/games/naiveplayer/NaivePlayer.java
@@ -1,28 +1,81 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package put.ai.games.naiveplayer;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
 
 public class NaivePlayer extends Player {
-    private final Random random = new Random(0xdeadbeef);
 
+    private long startTime = 0;
+    private long timeLimit = this.getTime();
+
+    private class MyMove {
+        private Move move;
+        private double score;
+        public MyMove(Move move, double score) {
+            this.move = move;
+            this.score = score;
+        }
+    }
 
     @Override
     public String getName() {
-        return "Gracz Naiwny 84868";
+        return "Mikołaj Skrzypczak 145398 Piotr Baryczkowski 145314";
     }
 
+    double score(Board board, Color color) {
+        if (board.getWinner(color) == color) return Double.MAX_VALUE;
+        int result = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board.getState(i, j) == color) result += 1;
+                else if (board.getState(i, j) != Color.EMPTY) result -= 1;
+            }
+        }
+        return result;
+    }
+
+    boolean timeout() {
+        return System.currentTimeMillis() - this.startTime >= this.timeLimit;
+    }
+
+    MyMove negMaxAlphaBeta( Board board, Color color, int turnMultiplier, int depth, double alpha, double beta) {
+        Color nextColor = (color == Color.PLAYER1) ? Color.PLAYER2 : Color.PLAYER1;
+
+        List<Move> nextMoves = board.getMovesFor(color);
+        Collections.shuffle(nextMoves);
+
+        if (depth == 0 || nextMoves.isEmpty() || timeout())
+            return new MyMove(null, turnMultiplier * score(board, color));
+
+        double value = Double.MIN_VALUE;
+        Move chosenMove = null;
+        for (Move move : nextMoves) {
+            board.doMove(move);
+            MyMove newMoveAndEval = negMaxAlphaBeta(board, nextColor, -turnMultiplier, depth - 1, -beta, -alpha);
+            board.undoMove(move);
+
+            chosenMove = move;
+            value = Math.max(value, -newMoveAndEval.score);
+            alpha = Math.max(value, alpha);
+
+            if (alpha >= beta) {
+                break;
+            }
+        }
+        return new MyMove(chosenMove, alpha);
+    }
+
+    Move getMyMove(Board board, Color color, int depthLimit) {
+        return negMaxAlphaBeta(board, color, 1, depthLimit, Double.MIN_VALUE, Double.MAX_VALUE).move;
+    }
 
     @Override
-    public Move nextMove(Board b) {
-        List<Move> moves = b.getMovesFor(getColor());
-        return moves.get(random.nextInt(moves.size()));
+    public Move nextMove(Board board) {
+        this.timeLimit = this.getTime() - 500;
+        this.startTime = System.currentTimeMillis();
+        return this.getMyMove(board, getColor(),  2);
     }
 }
